86cc661f17ce46608587ec3cdba254845ca11bc3
KorielZeth <110422274+KorielZeth@users.noreply.github.com>
2022-08-13 00:26:10 +0200

    keygen, keyimport, and filehandles work

--- rware/Source.cpp
+++ rware/Source.cpp
@@ -6,15 +6,21 @@
 
 
 
-int main() {
+int wmain() {
 
-	
+
 	HCRYPTPROV prov = NULL;
 	HCRYPTKEY clé = NULL;
-	DWORD bloblen = 0;
-	bool ok;
+	//DWORD bloblen = 0;
+	//bool ok = 0;
 
-	if (CryptAcquireContextW(&prov, 0, 0, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
+	BYTE plaintextkey[] = {
+	0x08,0x02,0x00,0x00,0x0f,0x66,0x00,0x00,
+	0x18,0x00,0x00,0x00,
+	0x3e,0x30,0x24,0x7c,0x48,0xa4,0x0f,0x82, 0xfd,0x7c,0xa5,0x1c,0xf6,0x6c,0x07,0xb7,0x68,0x60,0x47,0x86,0xc7,0xb9,0xba,0xab
+	};
+
+	/*if (CryptAcquireContextW(&prov, 0, 0, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
 
 		std::cout << "Context acquired" << std::endl;
 	}
@@ -37,7 +43,6 @@ int main() {
 
 	}
 
-
 	if (CryptExportKey(clé, 0, PLAINTEXTKEYBLOB, 0, NULL, &bloblen)) {
 
 		std::cout << "Size of the blob determined : " << bloblen << std::endl;
@@ -48,7 +53,6 @@ int main() {
 		return -1;
 	}
 
-	
 	BYTE blob[36];
 	ok = CryptExportKey(clé, 0, PLAINTEXTKEYBLOB, 0, blob, &bloblen);
 
@@ -66,11 +70,11 @@ int main() {
 		bytefile.write((const char*)blob,sizeof(blob));
 		bytefile.close();
 
-	}
+	}*/
 
 	// import key
-	
-	/*if (CryptAcquireContextW(&prov, 0, 0, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
+
+	if (CryptAcquireContextW(&prov, 0, 0, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
 
 		std::cout << "Context acquired" << std::endl;
 	}
@@ -80,7 +84,7 @@ int main() {
 		return -1;
 	}
 
-	if (CryptImportKey(prov,blob, sizeof(clé), 0, 0, &clé)) {
+	if (CryptImportKey(prov, plaintextkey, sizeof(plaintextkey), 0, 0, &clé)) {
 
 		std::cout << "Key succesfully imported" << std::endl;
 	}
@@ -89,57 +93,79 @@ int main() {
 		DWORD d = GetLastError();
 		std::cout << "Failure to import key, error code: " << d << std::endl;
 		return -1;
-	}*/
-
-	//File handling 
-	/*
-	const wchar_t* sourcefile = L"C:\\Users\\garma\\source\\repos\\rware\\x64\\Debug\\testfile.txt";
-	const wchar_t* destfile = L"C:\\Users\\garma\\source\\repos\\rware\\x64\\Debug\\testfile2.txt";
-	HANDLE sourcehandle = NULL;
-	HANDLE desthandle = NULL;
-
-
-	sourcehandle = CreateFileW(sourcefile, FILE_READ_DATA, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	desthandle = CreateFileW(destfile, FILE_WRITE_DATA | DELETE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	if (sourcehandle == INVALID_HANDLE_VALUE) {
-		DWORD d = GetLastError();
-		std::cout << "Error code : " << d << std::endl;
 	}
 
-	LPVOID buffer = NULL;
-	DWORD count = 0;
-	DWORD blocklen = 1280;
-	DWORD bufferlen = 1280;
-	int ending = 0;
+	//File handling 
 
-	while (ending == 0) {
-		if (ReadFile(sourcehandle, buffer, blocklen, &count, NULL) == 0) {
+	LPCWSTR pzSourceFile = L"C:\\Users\\Nono\\source\\repos\\rware\\x64\\Debug\\testfile.txt";
+	LPCWSTR pzDestFile = L"C:\\Users\\Nono\\source\\repos\\rware\\x64\\Debug\\testfile2";
 
-			std::cout << "Error reading from file" << std::endl;
-			break;
+	HANDLE hSourceFile = INVALID_HANDLE_VALUE;
+	HANDLE hDestFile = INVALID_HANDLE_VALUE;
 
-		}
-		std::cout << "Byte count : " << count;
-		if (count < blocklen) {
 
-			ending = 1;
-		}
-		if (CryptEncrypt(clé, 0, ending, 0, (BYTE*)buffer, &count, bufferlen) == 0) {
+	DWORD eof = 0;
+	PBYTE pbBuffer[960];
+	DWORD dwBlockLen = 960;
+	DWORD dwBufferLen = 960;
+	DWORD dwCount = 0;
+
+
+	hSourceFile = CreateFileW(pzSourceFile, FILE_READ_DATA, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hSourceFile != INVALID_HANDLE_VALUE) {
+
+		std::cout << "The sourcefile is open" << std::endl;
+	}
+	else {
+		DWORD d = GetLastError();
+		std::cout << "Error opening handle to the sourcefile, error code: " << d << std::endl;
+		return -1;
+
+	}
+
+	hDestFile = CreateFileW(pzDestFile, FILE_WRITE_DATA | DELETE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (INVALID_HANDLE_VALUE != hDestFile) {
+
+		std::cout << "The destfile is open" << std::endl;
+	}
+	else {
+		DWORD d = GetLastError();
+		std::cout << "Error opening handle to the destfile, error code 0x8" << d << std::endl;
+		return -1;
+
+	}
+
+	while (eof == 0) {
+
+		if (ReadFile(hSourceFile, pbBuffer, dwBlockLen, &dwCount, NULL) == 0) {
 
 			DWORD d = GetLastError();
-			std::cout << "Failed to encrypt, error code 0x" << count << " , last error : " << d << std::endl;
+			std::cout << "Error reading from the sourcefile, error code 0x8:" << d << std::endl;
 			break;
-		}
-		if (WriteFile(desthandle, buffer, count, &count, NULL) == 0) {
 
-			std::cout << "Error writing to file" << std::endl;
+		}
+
+		std::cout << dwCount << std::endl;
+
+		if (CryptEncrypt(clé, 0, eof, 0, (BYTE*)pbBuffer, &dwCount, dwBufferLen) == 0) {
+
+			DWORD d = GetLastError();
+			std::cout << "Error encrypting the buffer, error code 0x8" << d << std::endl;
 			break;
-		}
-		CloseHandle(sourcehandle);
-		CloseHandle(desthandle);
 
-	}*/
+		}
+
+		if (WriteFile(hDestFile, pbBuffer, dwCount, &dwCount, NULL) == 0) {
+
+			DWORD d = GetLastError();
+			std::cout << "Error writing to the destfile, error code 0x8:" << d << std::endl;
+			break;
+
+
+		}
+
+	}
+
 
 	return 0;
 }
